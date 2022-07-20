43532c418cb0e3a1c2d9a9137005bb7a367dc869
Kartikaya Gupta <kartikaya@squareup.com>
2022-07-20 12:32:14 -0400

    Drop subproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,36 @@ buildscript {
     repositories {
         mavenCentral()
     }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-    }
 }
 
-subprojects {
-    buildscript {
-        repositories {
-            mavenCentral()
-        }
+plugins {
+    kotlin("jvm") version "1.7.0"
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-kotlin:3.19.2")
+}
+
+tasks{
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.freeCompilerArgs += listOf(
+      "-opt-in=kotlin.RequiresOptIn",
+      "-opt-in=com.google.protobuf.kotlin.OnlyForUseByGeneratedProtoCode",
+      "-Xlint:-deprecation"
+    )
+  }
+}
+
+sourceSets {
+  main {
+    java {
+      srcDirs("src/main/java")
+      srcDirs("src/main/kotlin")
     }
-    repositories {
-        mavenCentral()
-    }
+  }
 }
